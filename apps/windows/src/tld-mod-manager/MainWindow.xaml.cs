8e82345fcee849948bb747920a1e6a6af6c4c741
Deaadman <47807486+Deaadman@users.noreply.github.com>
2026-01-12 14:43:16 +1030

    Navigation menu toggling, and localisation

--- apps/windows/src/tld-mod-manager/MainWindow.xaml.cs
+++ apps/windows/src/tld-mod-manager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using DevWinUI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using TitleBar = Microsoft.UI.Xaml.Controls.TitleBar;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -15,13 +17,34 @@ namespace tld_mod_manager
         {
             InitializeComponent();
             ConfigureTitleBar();
+            SetNavigationMenuLocalisations();
         }
 
+        #region TitleBar
         private void ConfigureTitleBar()
         {
             this.ExtendsContentIntoTitleBar = true;
+            this.AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
             this.SetTitleBar(TitleBar);
             TitleBar.Title = "ProjectName".GetLocalizedResource();
         }
+
+        #region Events
+        private void TitleBar_OnPaneToggleRequested(TitleBar sender, object args)
+        {
+            NavigationView.IsPaneOpen = !NavigationView.IsPaneOpen;
+        }
+        #endregion
+        #endregion
+
+        #region Navigation Menu
+        private void SetNavigationMenuLocalisations()
+        {
+            Home.Content = "Home".GetLocalizedResource();
+            Mods.Content = "Mods".GetLocalizedResource();
+            Downloads.Content = "Downloads".GetLocalizedResource();
+            Library.Content = "Library".GetLocalizedResource();
+        }
+        #endregion
     }
 }
