ecdc0d01264cbcb75733b3fd6bba87bc16d58675
Deaadman <47807486+deaadman@users.noreply.github.com>
2026-01-19 10:36:30 +1030

    Quick navigation bar setup

---
+++ web/src/lib/components/navigation/navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import ThemeSwitcher from "$lib/components/theme/theme-switcher.svelte";
+</script>
+
+<div class="p-4 bg-blue-500">
+    <div class="grid grid-flow-col grid-cols-3 gap-4 items-center">
+        <div class="justify-self-start">
+            <a class="flex items-center" href="/">
+                <img src="src/lib/assets/favicon.svg" alt="The Long Dark Modding Logo" height="48" width="48"/>
+                <h3 class="ms-4 scroll-m-20 text-2xl font-semibold tracking-tight">The Long Dark Modding</h3>
+            </a>
+        </div>
+        <div class="justify-self-center">
+<!--            <p>Hello</p>-->
+        </div>
+        <div class="justify-self-end">
+            <ThemeSwitcher />
+        </div>
+    </div>
+</div>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
+    import { ModeWatcher } from "mode-watcher";
 	import favicon from '$lib/assets/favicon.svg';
+    import Navbar from "$lib/components/navigation/navbar.svelte";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<Navbar />
 {@render children()}
