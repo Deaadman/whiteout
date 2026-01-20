aa77a903a18257e31f25fde42d81723fca7d8388
Deaadman <47807486+Deaadman@users.noreply.github.com>
2026-01-20 20:54:04 +1030

    Finished help page

--- web/src/routes/help/+page.svelte
+++ web/src/routes/help/+page.svelte
@@ -4,51 +4,39 @@
 </script>
 
 <div class="grid grid-cols-2 gap-2 py-2">
-    <Card.Root>
+    <Card.Root class="max-md:col-span-2">
         <Card.Content>
             <h2 class="scroll-m-20 mb-4 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Installing Mods Guide</h2>
             <p class="mb-4">Mods are only compatible / supported with Steam versions of the game.</p>
             <p class="mb-4">This website is in no way affiliated with Hinterland Games and only aims to provide easy access to published mods for the game.</p>
             <ol class="my-6 ms-6 list-decimal [&>li]:mt-2">
                 <li>
-                    If you haven't turned off sharing crash reports and analytics, launch TLD and turn these off first.
+                    If you have not turned off sharing crash reports and analytics, launch The Long Dark and turn these off first.
                     <br/>
-                    It's important! Let's not bug Hinterland with issues unless you're certain it's their fault.
+                    <p class="text-muted-foreground text-sm">It's important! Let's not bug Hinterland with issues unless you're certain it's their fault.<p/>
                 </li>
                 <li>
                     Make sure to backup your saves before modding, mods are usually tested, but bad stuff happens sometimes.
                     <br/>
-                    You can find your saves in C:\Users\[your-windows-user]\AppData\Local\Hinterland\TheLongDark.
+                    <p class="text-muted-foreground text-sm">On Windows, you can find your saves in C:\Users\[your-windows-user]\AppData\Local\Hinterland\TheLongDark.</p>
                 </li>
                 <li>
-                    First of all, make sure you have the Melon Loader requirements installed. For current version of TLD these are:
-                    <ul class="ms-6 list-disc [&>li]:mt-2">
-                        <li>
-                            Microsoft Visual C++ 2015-2019 Redistributable 64 Bit
-                        </li>
-                        <li>
-                            .NET 6.0 Runtime.
-                        </li>
-                    </ul>
+                    First of all, make sure you have the <a class="underline" href="https://melonwiki.xyz/#/?id=requirements" target="_blank">MelonLoader requirements</a> installed.
                 </li>
                 <li>
                     Next, we need MelonLoader:
                     <ul class="ms-6 list-disc [&>li]:mt-2">
                         <li>
-                            Download MelonLoader here
+                            Download <a class="underline" href="https://melonwiki.xyz/#/?id=automated-installation" target="_blank">MelonLoader</a>.
                         </li>
                         <li>
                             Run MelonLoader.
                         </li>
                         <li>
-                            Select TheLongDark from the detected game list
-                            <br/>
-                            (or Add Game Manually if not detected)
+                            Select The Long Dark.
                         </li>
                         <li>
-                            Make sure to install the 0.7.2 version by selecting the version from the list
-                            <br/>
-                            (Make sure to tick "Enable Nightly Builds" and pick the latest)
+                            Make sure to install version 0.7.2-ci.2388. (Make sure 'nightly' is ticked).
                         </li>
                         <li>
                             Wait for the download and installation to complete!
@@ -56,26 +44,25 @@
                     </ul>
                 </li>
                 <li>
-                    Download any mods you want using the  icon, (extract the zip if packaged as one) and place the .dll/.modcomponent files inside the Mods folder that MelonLoader created in your game directory
-                    (Make sure to read any instructions for the mod, some contain multiple versions zipped up)
+                    Download any mods you want, (extract the zip if packaged as one) and place the .dll/.modcomponent files inside the Mods folder that MelonLoader created in your game directory.
                 </li>
                 <li>
-                    Make sure you've also installed any dependency the mod might need
+                    Make sure you have also installed any dependencies the mod might need.
                 </li>
                 <li>
-                    Launch TLD and enjoy your modded game :)
+                    Launch The Long Dark and enjoy :)
                 </li>
             </ol>
         </Card.Content>
     </Card.Root>
-    <Card.Root class="col-start-2">
+    <Card.Root class="max-md:row-start-2 max-md:col-span-2 col-start-2">
         <Card.Content>
             <h2 class="scroll-m-20 mb-4 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Frequently Asked Questions</h2>
             <Accordion.Root type="multiple">
                 <Accordion.Item value="item-1">
                     <Accordion.Trigger>MelonLoader errors with 'IL2CPP-init detour failed: one of the dependent libraries is missing'</Accordion.Trigger>
                     <Accordion.Content>
-                        You need to install .NET 6 Runtime.
+                        You need to install <a class="underline" href="https://dotnet.microsoft.com/en-us/download/dotnet/6.0" target="_blank">NET 6 Runtime</a>.
                     </Accordion.Content>
                 </Accordion.Item>
                 <Accordion.Item value="item-2">
